7cf6d9c40216e08538c509407904accddbb393ce
hunter-aw <hunteraweinsheink@gmail.com>
2018-07-23 16:04:45 +0300

    js exercise 1, drinking age

---
+++ week_1/day_2/running-js/script.js
@@ -0,0 +1,8 @@
+let userAge = prompt("Hey there partner, how old are ya?")
+
+if (userAge < 18) {
+    alert("Wo-ho-hoa there sonny, you're a little to young to be drinkin ain't ya? Why don't you come back in a couple years")
+} else { //I don't need to set an else if condition as anything that isn't <18 is by definition >= 18
+    alert("Alrighty Partner, let's get some moonshine in ya!")
+}
+
