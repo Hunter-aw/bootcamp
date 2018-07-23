76475cb8fff5e27d0a9c812aed9ef0b382bf10cc
hunter-aw <hunteraweinsheink@gmail.com>
2018-07-23 16:40:54 +0300

    fixed my or statement

--- week_1/day_2/running-js/script.js
+++ week_1/day_2/running-js/script.js
@@ -1,8 +1,13 @@
 let userAge = prompt("Hey there partner, how old are ya?")
+let userGender = prompt("Tell me good lookin' is you a man or a woo man?")
 
 if (userAge < 18) {
     alert("Wo-ho-hoa there sonny, you're a little to young to be drinkin ain't ya? Why don't you come back in a couple years")
-} else { //I don't need to set an else if condition as anything that isn't <18 is by definition >= 18
-    alert("Alrighty Partner, let's get some moonshine in ya!")
+} else if (userGender === "female" | "woman" | "woo man") {
+    alert("Alrighty Missy, drinks on our us tonight, darn tootin!!")
+} else if (userGender === "male" | "man") {
+    alert("Well partner, you can come on it, but I ain't payin for ya!")
+} else {
+    alert("What do you mean gender is a construct?")
 }
 
