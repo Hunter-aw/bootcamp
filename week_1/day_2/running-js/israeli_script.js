036d8032e8a44f3633f79ad751531a1b5b22a106
hunter-aw <hunteraweinsheink@gmail.com>
2018-07-23 16:32:09 +0300

    israeli border script + html

---
+++ week_1/day_2/running-js/israeli_script.js
@@ -0,0 +1,10 @@
+let israelite = prompt("em, how you say, welcome to Israel. Tell me, em, from which nationality are you?")
+let isl = "Israeli"// /i;
+// let isrl = isl.ignoreCase
+
+
+if (israelite === isl) {
+    alert("yalla, bo")
+} else {
+    alert("em, I am sorry, this is not the place for you, please, em, go and, how you say, leave")
+}
