5b43ad6c68b59b37d1b2b413f9ffec21712706ca
hunter-aw <hunteraweinsheink@gmail.com>
2018-07-25 17:15:16 +0300

    objects lesson exercises

---
+++ week_1/day_4/objects_lesson.js
@@ -0,0 +1,11 @@
+const car = {
+    wheels: 4,
+    color: "fire-red"
+}
+
+const user = {
+    name: "Hunter",
+    age: 23,
+    email: "hunteraweinsheink@gmail.com",
+    interests: ["kayaking", "biking", "swiming"]
+}
