d92498b049adf300026d46587ad0ea988bbdb3d9
hunter-aw <hunteraweinsheink@gmail.com>
2018-07-26 13:50:32 +0300

    kept trying this case sensitive shit

--- week_1/day_4/reservations/main.js
+++ week_1/day_4/reservations/main.js
@@ -7,18 +7,27 @@ var reservations = {
   }
 }
 
-// var oldName = prompt('Please enter the name for your reservation');
+var name = prompt('Please enter the name for your reservation');
 // let name =  new RegExp(oldName, "i");
 
-var claimReservation = function () {  
+// newReservations = reservations.keys()
+// var reservationNamer = function () {
+//   for (let newName of newReservations) {
+//     newName = newName.toLowerCase()
+//   } return newReservations
+// }
+
+var claimReservation = function () {
   if ((reservations[name]) && (reservations[name].claimed) === false) {
     return (alert("Welcome to George and Sons, we have your table ready"))
   } else if ((reservations[name]) && (reservations[name].claimed === true)) {
     return alert("well this is awkward, someone's taken your table")
   } else {
-    reservations[name] = {claimed: true}
+    reservations[name] = {
+      claimed: true
+    }
     return alert(`We have plentry of room, come on in ${name}!`)
   }
 }
-
+// reservationNamer();
 claimReservation();
