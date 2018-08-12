3659a724eedb98344b3ecdbe0669cb95e86d2833
hunter-aw <hunteraweinsheink@gmail.com>
2018-08-12 16:45:17 +0300

    node.js practice yay so fun

---
+++ week_4/day_1/node-test/rectangleUtils.js
@@ -0,0 +1,8 @@
+var title = "Rectangle Utility";
+
+var calcRectangleArea = function (length, height) {
+  return length*height;
+};
+
+module.exports.calcRectangleArea = calcRectangleArea;
+module.exports.title = title
---
+++ week_4/day_1/node-test/shapesApp.js
@@ -0,0 +1,7 @@
+var circleUtils = require('./circleUtils');
+console.log(circleUtils.title);
+console.log(circleUtils.calcCircleArea(10));
+console.log("Just to verify: " + 10 * 10 * 3.14159);
+var rectangleUtils = require('./rectangleUtils').calcRectangleArea
+// console.log(rectangleUtils.title)
+console.log(rectangleUtils(2,4))
