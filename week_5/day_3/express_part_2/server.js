486942de167e6b2fc7dcbcf29ee2988d6640435c
hunter-aw <hunteraweinsheink@gmail.com>
2018-08-21 22:26:05 +0300

    worked on express 2 lesson

---
+++ week_5/day_3/express_part_2/server.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const app = express()
+
+// var myLogger = function (req, res, next) {
+//     console.log('LOGGED');
+//     next();
+// };
+
+// var toLower = function (req, res, next) {
+//     console.log('ayyyy');
+//     next();
+// }
+
+// app.use(function (req, res, next) {
+//     req.requestTime = new Date().toString();
+//     next();
+//   });
+  
+//   app.get('/', function (req, res) {
+//     var responseText = 'Requested at: ' + req.requestTime + '. Like a boss! ';
+//     res.send(responseText);
+//   });
+
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/public/test.html')
+  });
+
+app.get('/', function (req, res) {
+  res.send('This is from the root directory')
+});
+
+app.use(function(req, res, next){
+    res.status(404);
+  
+    // respond with html page
+    if (req.accepts('html')) {
+      res.send('404'); //write cool 404 html here
+      return;
+    }
+});
+
+
+
+app.listen(1212, () => {
+    console.log('surver is surviving at http://localhost:1212')
+});
+
