6de27b995531951e26960f4dae5962feae5dc203
hunter-aw <hunteraweinsheink@gmail.com>
2018-08-27 17:59:22 +0300

    promises practice kinda

--- week_6/promises/promises.js
+++ week_6/promises/promises.js
@@ -1,15 +1,34 @@
-var isThereBurgerLeft = false;
+var areThereBunsLeft = true;
+var isThereMeatLeft = true;
+var money = 10
+var burgerPrice = 20
+
+var isThereBurgerLeft = () => (areThereBunsLeft && isThereMeatLeft)
 
 //Promise
 var willWeGetaBurger = new Promise(
-    function(resolve, reject) {
-        if (isThereBurgerLeft) {
-            resolve('Yum yum!'); // fulfilled
-        } else {
-            reject('hmmm... pizza instead'); reject
-        }
+    function (resolve, reject) {
+        setTimeout(function () {
+            if (isThereBurgerLeft()) {
+                resolve('Yum yum!'); // fulfilled
+            } else {
+                reject('hmmm... pizza instead');
+            }
+        }, 2000)
     }
 );
+
+var canIAffordTheBurger = new Promise(
+    function (res, rej) {
+        if (money > burgerPrice) {
+            res("enjoy your burger!")
+        } else {
+            rej("ur broke! geddout of here!")
+        }
+    }
+)
+
+// var 
 // That is a promise... 
 // An object that receives 2 parameters resolve, reject (similar to success, error)
 // promise syntax look like this
@@ -17,6 +36,13 @@ var willWeGetaBurger = new Promise(
 
 //call our promise
 
-var whatCanWeEat = function() {
-    
-}
+var whatCanWeEat = function () {
+    willWeGetaBurger //
+        .then(canIAffordTheBurger
+            .then(function (fulfilled) {
+            //yay, burger!
+            console.log(fulfilled);
+        }));
+};
+
+whatCanWeEat(); //invoke function that will call our promise
