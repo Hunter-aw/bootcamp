1549feb6220451d6a9b62dff18e36111fe65578c
hunter-aw <hunteraweinsheink@gmail.com>
2018-10-20 10:07:47 +0300

    partway through merge sory

--- sort.js
+++ sort.js
@@ -19,28 +19,87 @@ const arr2 = [1, 36, 9, 9, 25, 36, 64, 16, 16, 16, 9, 9]
 //     let arr2 = arr.splice(half,half)
 //     console.log(arr1)
 // }
-miniSort = (master, i, x) => { 
-    if (master[x]) {
-    let combined = master[i].concat(master[x])
-    for (i in combined){
-        let x = parseInt(i, 10)+1
-        if (combined[x]<combined[i]) {
-            combined.splice(i, 0, combined[x])
-            combined.splice(x+1,1)
+// miniSort = (master, i, x) => { 
+//     if (master[x]) {
+//     let combined = master[i].concat(master[x])
+//     for (i in combined){
+//         let x = parseInt(i, 10)+1
+//         if (combined[x]<combined[i]) {
+//             combined.splice(i, 0, combined[x])
+//             combined.splice(x+1,1)
+//         }
+//     } 
+//     return combined
+//     }
+// }
+
+// mergeSort = () => {
+//     let master = arr.map(num => [num])
+//     let newMaster = []
+//     for (let i in master) {miniSort = (master, i, x) => { 
+//     if (master[x]) {
+//     let combined = master[i].concat(master[x])
+//     for (i in combined){
+//         let x = parseInt(i, 10)+1
+//         if (combined[x]<combined[i]) {
+//             combined.splice(i, 0, combined[x])
+//             combined.splice(x+1,1)
+//         }
+//     } 
+//     return combined
+//     }
+// }
+
+// mergeSort = () => {
+//     let master = arr.map(num => [num])
+//     let newMaster = []
+//     for (let i in master) {
+//         let x = parseInt(i, 10)+1
+//         let combined = miniSort(master, i, x)
+//         newMaster.concat(combined)
+//     }
+//     console.log(master)
+// }
+//         let x = parseInt(i, 10)+1
+//         let combined = miniSort(master, i, x)
+//         newMaster.concat(combined)
+//     }
+//     console.log(master)
+// }
+const heapSort = (array) => {
+    if (array.length > 1) {
+        let temp = array.splice(0,1)
+        console.log(array)
+        heapSort(array)
+        let i = 0
+        while (array[i] < temp[0]) {
+            i++
         }
-    } 
-    return combined
+        array.splice(i, 0, temp[0])
+        console.log(array)
+    }
+    return;
+
+}
+
+// heapSort(arr1)
+
+const mergeSort = (array) => {
+    const half = Math.floor(array.length/2)
+    
+    if (ray1.length === 0 || ray2.length === 0) {
+        return;
+    }
+    const ray1 = array.slice(0, half)
+    const ray2 = array.slice(half, array.length-1)
+
+    const rayray1 = mergeSort(ray1)
+    const rayray2 = mergeSort(ray2)
+
+    const mergedArray = []
+    while(mergedArray.length < array.length) {
+        
     }
 }
 
-mergeSort = () => {
-    let master = arr.map(num => [num])
-    let newMaster = []
-    for (let i in master) {
-        let x = parseInt(i, 10)+1
-        let combined = miniSort(master, i, x)
-        newMaster.concat(combined)
-    }
-    console.log(master)
-}
-mergeSort()
+mergeSort(arr1)
